ee2deb4eed88616dc1d0c69ac14ec668a727dd8d
Александра <alexa@MacBook-Air-Aleksandra.local>
2019-10-12 09:02:18 +0300

    Models

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const video = document.getElementById('video');
+const video = document.getElementById('video')
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-]).then(startVideo);
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
 function startVideo() {
   navigator.getUserMedia(
@@ -16,17 +16,22 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
 
-  const displaySize = { width: video.width, height: video.height}
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
 
   setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+    const detections = await faceapi.detectAllFaces(video, new faceapi
+      .TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions()
 
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
   }, 100)
 })
